77f3064ae66116023f578a6f2687f47d173dff82
Janb-111 <jan.barlewicz@gmail.com>
2020-10-19 17:17:16 +0200

    Add new files

--- Main.java
+++ Main.java
@@ -10,12 +10,18 @@ class PeopleStats {
 
 class Person {
     private final String firstName;
+    private final String lastName;
 
-    public Person(String firstName) {
+    public Person(String firstName, string lastName) {
         this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     public String getFirstName() {
         return firstName;
+    }
+
+    public String getlastName() {
+        return lastName;
     }
 }
