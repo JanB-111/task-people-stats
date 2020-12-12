d26f4ed801ee2ae20dd334de71df7f6c5fd67939
Janb-111 <jan.barlewicz@gmail.com>
2020-12-12 20:04:48 +0100

    final

--- Main.java
+++ Main.java
@@ -8,6 +8,13 @@ class Main {
 
         System.out.println(String.format("Liczba osób: %d", peopleStats.count()));
     }
+
+    public static void main(String[] args) {
+        PeopleStats peopleStats = new PeopleStats(Paths.get("śćieżka", "do", "pliku"));
+
+        System.out.println(String.format("Liczba osób: %d", peopleStats.count()));
+        System.out.println(String.format("Liczba osób z unikalnymi nazwiskami: %d", peopleStats.countUniqueLastNames()));
+    }
 }
 
 
@@ -31,6 +38,13 @@ class PeopleStats {
 
     public long count() {
         return people.size();
+    }
+
+    public long countUniqueLastNames() {
+        return people.stream()
+                .map(Person::getLastName)
+                .distinct()
+                .count();
     }
 }
 
